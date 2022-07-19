4f872503c3458fd4aa813db388b8f96743d9ffae
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-07-19 00:49:41 -0300

    Criar arquivo de transação de usuário

---
+++ lib/components/transaction_user.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'transaction_form.dart';
+import 'transection_list.dart';
+import '../models/transaction.dart';
+
+import '../models/transaction.dart';
+
+class TransactionUser extends StatefulWidget {
+  const TransactionUser({Key? key}) : super(key: key);
+
+  @override
+  State<TransactionUser> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 110.30,
+      date: DateTime.now(),
+    )
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransactionList(_transactions),
+        TransactionForm(),
+      ],
+    );
+  }
+}
