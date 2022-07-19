de69542a758c0b72b91d60811e99f99b6fa4bc11
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-07-19 00:25:01 -0300

    Remover Card

--- lib/main.dart
+++ lib/main.dart
@@ -47,43 +47,7 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           TransactionList(_transactions),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: titleController,
-                    decoration: InputDecoration(
-                      labelText: 'Título',
-                    ),
-                  ),
-                  TextField(
-                    controller: valueController,
-                    decoration: InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(
-                        style: TextButton.styleFrom(
-                          primary: Colors.purple,
-                        ),
-                        child: const Text('Nova Transação'),
-                        onPressed: () {
-                          print(titleController.text);
-                          print(valueController.text);
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
+          
         ],
       ),
     );
