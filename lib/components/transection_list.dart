26ae835a3b5c7cfd49986885040c524b7ff46b12
DavidCavalcanti <54603847+DavidCavalcanti@users.noreply.github.com>
2022-07-19 00:12:15 -0300

    Criar pasta para dividir app em Widgets

---
+++ lib/components/transection_list.dart
@@ -0,0 +1,14 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+class TransactionList extends StatelessWidget {
+  late final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
